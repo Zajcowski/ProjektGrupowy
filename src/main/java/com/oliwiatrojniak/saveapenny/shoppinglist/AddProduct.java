efa919012d1c4cfffd78d44937bfff6048018305
Jan Rojek <janekrojek683@gmail.com>
2025-05-23 23:50:32 +0200

    shopping list
    added category, better style, visibility to see price and quantity of each item in list

--- src/main/java/com/oliwiatrojniak/saveapenny/shoppinglist/AddProduct.java
+++ src/main/java/com/oliwiatrojniak/saveapenny/shoppinglist/AddProduct.java
@@ -2,19 +2,26 @@ package com.oliwiatrojniak.saveapenny.shoppinglist;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class AddProduct {
 
   String name;
   float price;
   int quantity;
+  String category;
 
-  public AddProduct(String name, float price, int quantity){
+  public AddProduct(String name, float price, int quantity, String category){
     this.name = name;
     this.price = price;
     this.quantity = quantity;
+    this.category = category;
   }
 
+  public AddProduct(String name, float price, int quantity){
+    this(name, price, quantity, null);
+  }
 
 }
